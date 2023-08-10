504d58ec774901be1f41f6da71af931dc4daea7d
Rasul Rukh <rassrukh@Rasuls-MacBook-Pro.local>
2023-08-11 03:22:38 +0500

    analyzing the data

--- program/main.py
+++ program/main.py
@@ -1,2 +1,47 @@
-if __name__ ==  "__main__":
-    print("Hi there")
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
+from func_connection import connect_dydx
+from func_private import abort_all_positions
+from func_public import construct_market_prices
+from func_cointegration import store_cointegration_results
+
+
+if __name__ == "__main__":
+    # Connect to client
+    try:
+        client = connect_dydx()
+    except Exception as e:
+        print(e)
+        print("Error connecting to client: ", e)
+        exit(1)
+
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all positions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions: ", e)
+            # send_message(f"Error closing all positions {e}")
+            exit(1)
+
+    # Find Cointegrated Pairs
+    if FIND_COINTEGRATED:
+        # Construct Market Prices
+        try:
+            print("Fetching market prices, please allow 3 mins...")
+            df_market_prices = construct_market_prices(client)
+        except Exception as e:
+            print("Error constructing market prices: ", e)
+            #   send_message(f"Error constructing market prices {e}")
+            exit(1)
+
+        # Store Cointegrated Pairs
+        try:
+            print("Storing cointegrated pairs...")
+            stores_result = store_cointegration_results(df_market_prices)
+            if stores_result != "saved":
+                print("Error saving cointegrated pairs")
+                exit(1)
+        except Exception as e:
+            print("Error saving cointegrated pairs: ", e)
+            #   send_message(f"Error saving cointegrated pairs {e}")
+        exit(1)
